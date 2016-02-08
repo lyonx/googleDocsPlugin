2fa6a226a167118afa37a088f5dcbbd554d1e93c
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-02-08 16:43:56 +0530

    Showed google doc in widget section. Added onUpdate handler on datastore also.

---
+++ widget/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+(function (angular) {
+  angular.module('googleAppsDocPluginWidget', ['ui.bootstrap'])
+    .controller('WidgetHomeCtrl', ['$scope', 'Buildfire', 'DataStore', 'TAG_NAMES', 'STATUS_CODE',
+      function ($scope, Buildfire, DataStore, TAG_NAMES, STATUS_CODE) {
+        var WidgetHome = this;
+        /*
+         * Fetch user's data from datastore
+         */
+        WidgetHome.init = function () {
+          WidgetHome.success = function (result) {
+            WidgetHome.data = result.data;
+            if (!WidgetHome.data.content)
+              WidgetHome.data.content = {};
+            console.log(">>>>>", WidgetHome.data);
+          };
+          WidgetHome.error = function (err) {
+            if (err && err.code !== STATUS_CODE.NOT_FOUND) {
+              console.error('Error while getting data', err);
+            }
+          };
+          DataStore.get(TAG_NAMES.GOOGLE_DOC_INFO).then(WidgetHome.success, WidgetHome.error);
+        };
+
+        WidgetHome.onUpdateCallback = function (event) {
+          if (event && event.tag === TAG_NAMES.GOOGLE_DOC_INFO) {
+            WidgetHome.data = event.data;
+            if (WidgetHome.data && !WidgetHome.data.content)
+              WidgetHome.data.content = {};
+          }
+        };
+
+        DataStore.onUpdate().then(null, null, WidgetHome.onUpdateCallback);
+        WidgetHome.init();
+
+      }])
+    .filter('returnUrl', ['$sce', function ($sce) {
+      return function (url) {
+        return $sce.trustAsResourceUrl(url);
+      }
+    }]);
+})(window.angular);
